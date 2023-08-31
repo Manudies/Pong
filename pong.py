924c3f26cc25cf0212af3fbf2059213ec01f8374
Tony G. Bolaño <tony@tonygb.com>
2023-08-31 21:33:28 +0200

    feat: toma de contacto con pygame

--- pong.py
+++ pong.py
@@ -1,1 +1,24 @@
 import pygame
+
+pygame.init()
+pantalla = pygame.display.set_mode((800, 600))
+
+salir = False
+while not salir:
+    # bucle principal (o main loop)
+
+    for evento in pygame.event.get():
+        print('Se ha producido un evento de tipo', evento.type)
+        if evento.type == pygame.QUIT:
+            print('Se ha cerrado la ventana')
+            salir = True
+
+
+    # renderizar nuestros objetos
+    rectangulo = pygame.Rect(50, 100, 300, 150)
+    pygame.draw.rect(pantalla, (94, 68, 158), rectangulo)
+
+    # mostrar los cambios en la pantalla
+    pygame.display.flip()
+
+pygame.quit()
