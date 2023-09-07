54b7c234200cbd8564c9d42cf3b65510ec43c819
Your Name01~clear <manudies@gmail.com>
2023-09-07 21:25:25 +0200

    feat: Marcador en pantalla

--- pong.py
+++ pong.py
@@ -2,7 +2,6 @@
 from random import randint
 import pygame
 
-
 ANCHO = 1280
 ALTO = 720
 
@@ -14,13 +13,14 @@ ALTO_PALA = 50
 MARGEN = 20
 TAM_PELOTA = 10
 
-VEL_MAX = 5
+VEL_MAX = 10
 VEL_JUGADOR = 5
 
 ARRIBA = True
 ABAJO = False
 
-FPS = 60
+FPS = 35
+TAM_LETRA = 100
 
 
 class Pelota(pygame.Rect):
@@ -48,9 +48,22 @@ class Pelota(pygame.Rect):
 
     def comprobar_punto(self):
         # Comprobar si la pelota ha salido por los laterales
-        # Si sale por la derecha  suma a jugador 1
+
         # Si sale por la izquierda suma a jugador 2
-        pass
+        if self.right <= 0:
+            print("Punto para J2")
+            self.center = (ANCHO/2, ALTO/2)
+            self.velocidad_y = randint(-VEL_MAX, VEL_MAX)
+            self.velocidad_x = randint(-VEL_MAX, -1)
+            return 2
+        # Si sale por la derecha  suma a jugador 1
+        if self.left >= ANCHO:
+            print("Punto para J1")
+            self.center = (ANCHO/2, ALTO/2)
+            self.velocidad_y = randint(-VEL_MAX, VEL_MAX)
+            self.velocidad_x = randint(1, VEL_MAX)
+            return 1
+        return 0
 
 
 class Jugador(pygame.Rect):
@@ -73,15 +86,58 @@ class Jugador(pygame.Rect):
                 self.y += VEL_JUGADOR
 
 
+class Marcador:
+    def __init__(self):
+        self.reset()
+        self.tipografia = pygame.font.SysFont('candara', TAM_LETRA)
+
+    def incrementar(self, jugador):
+        self.puntos[jugador-1] += 1
+
+    def reset(self):
+        self.puntos = [0, 0]
+
+    def pintame(self, pantalla):
+
+        # puntuacion = str(self.puntos[0])
+        # texto = self.tipografia.render(puntuacion, True, C_OBJETOS)
+        # pos_x = ANCHO/4
+        # pos_y = MARGEN
+        # pantalla.blit(texto, (pos_x, pos_y))
+        i = 1
+        for punto in self.puntos:
+            puntuacion = str(punto)
+            texto = self.tipografia.render(puntuacion, True, C_OBJETOS)
+            pos_x = i * ANCHO/4
+            pos_y = MARGEN
+            pantalla.blit(texto, (pos_x, pos_y))
+            i += 2
+
+    def comprobar_ganador(self):
+        pass
+
+        """
+    NEcesita:
+    - Atributo Guardar la puntuación J1
+    - Atributo Guardar la puntuación J2
+    - Metodo ponerse a cero
+    - Metodo mostrarse en pantalla"""
+
+
 class Pong:
-    def __init__(self) -> None:
+    def __init__(self):
         pygame.init()
         self.pantalla = pygame.display.set_mode((ANCHO, ALTO))
+        self.nombre_ventana = pygame.display.set_caption("Manu's Pong")
+        # self.cargar_icono = pygame.image.load("icono.png")
+        # self.icono = pygame.display.set_icon(self.cargar_icono)
         self.reloj = pygame.time.Clock()
 
         self.pelota = Pelota()
+
         self.jugador1 = Jugador(MARGEN, (ALTO-ALTO_PALA)/2)
         self.jugador2 = Jugador(ANCHO - MARGEN, (ALTO-ALTO_PALA)/2)
+        self.marcador = Marcador()
 
     def jugar(self):
         # contiene el bucle principal
@@ -92,23 +148,28 @@ class Pong:
             # bucle principal (o main loop)
             # Boque 1 Captura de eventos
             for evento in pygame.event.get():
-                if evento.type == pygame.QUIT:
+                if evento.type == pygame.QUIT or (evento.type == pygame.KEYUP and evento.key == pygame.K_ESCAPE):
                     salir = True
 
             self.comprobar_teclas()
 
             # BLoque 2 Renderizar nuestros objetos
-            # PANTALLA
             self.pantalla.fill(C_FONDO)
             # pygame.draw.rect(self.pantalla, C_FONDO, ((0, 0), (ANCHO, ALTO)))
-
             self.pintar_red()
+
             self.jugador1.pintame(self.pantalla)
             self.jugador2.pintame(self.pantalla)
 
             self.pintar_pelota()
+            hay_punto = self.pelota.comprobar_punto()
+            if hay_punto > 0:
+                self.marcador.incrementar(hay_punto)
+                hay_ganador = self.marcador.comprobar_ganador()
 
-            # mostrar los cambios en la pantalla
+            self.marcador.pintame(self.pantalla)
+
+            # Bloque 3: mostrar los cambios en la pantalla
             pygame.display.flip()
             self.reloj.tick(FPS)
         pygame.quit()
@@ -126,7 +187,8 @@ class Pong:
 
     def pintar_pelota(self):
         self.pelota.mover()
-
+        if self.pelota.colliderect(self.jugador1) or self.pelota.colliderect(self.jugador2):
+            self.pelota.velocidad_x = -self.pelota.velocidad_x
         self.pelota.pintame(self.pantalla)
 
     def pintar_red(self):
